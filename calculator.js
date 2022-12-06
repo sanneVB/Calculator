f7e7bbd1a3c6f41711f3074c68d5327006de3776
sanneVB <sannevanboheemen@gmail.com>
2022-12-06 22:34:11 +0100

    Just simple addition and substraction functino for now

---
+++ calculator.js
@@ -0,0 +1,24 @@
+function addition(currentValue, newValue) {
+  let addedValue = currentValue + newValue;
+  addedValue = addedValue.toFixed(2);
+  return addedValue;
+}
+
+function subtraction(currentValue, newValue) {
+  let subractedValue = currentValue - newValue;
+  subractedValue = subractedValue.toFixed(2);
+  return subractedValue;
+}
+
+// tiles.forEach(
+//   // Tracks all the tiles and their index (0-8)
+//   (tileDivElement, i) =>
+//   // Tracks which tile has been clicked and returns the index
+//     tileDivElement.addEventListener('click', ((e) => {
+//       // Function that changes the input field
+//       addInput(i);
+//     })),
+// );
+
+console.log(addition(500.1547, -30156.15489496));
+console.log(subtraction(500.1547, -30156.15489496));
