94757137944d70b299b0bdf989875679474885f7
sanneVB <sannevanboheemen@gmail.com>
2022-12-12 14:39:00 +0100

    Updated layout
    Updated the way the layout is generated.
      Using an array to create the buttons dynamycally
      Using seperate array for function buttons
    
    Messed with the style so it looks cleaner.
      Now have three flexboxes
        One for the calculation field
        One for Number buttons
        One for Function Buttons
      Made the text significantly larger

--- calculator.js
+++ calculator.js
@@ -1,12 +1,24 @@
-const containerId = document.getElementById('container');
+// const containerId = document.getElementById('numberButtonContainer');
 
-function buttonCreation(symbol) {
+// Creating a list of symbols that should appear on the number buttons
+// Should be listed in the order that they appear from top left
+const numberButtonSymbols = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
+
+// Creating a list of symbols that should appear on the function buttons
+// Should be list from top to bottom
+const functionButtonSymbols = ['CE', '-', '+', '='];
+
+// Function that will take symbol input and then create a button with that symbol
+function buttonCreation(symbol, targetID) {
+  const containerId = document.getElementById(targetID);
+  // Creating seperate divs for each button to live
   const buttonDiv = document.createElement('div');
   buttonDiv.classList.add('button-div');
   buttonDiv.id = `button${symbol}`;
 
   containerId.appendChild(buttonDiv);
 
+  // Then creating the button inside each of those divs
   const buttonDivId = document.getElementById(`button${symbol}`);
 
   const calcButton = document.createElement('button');
@@ -16,21 +28,27 @@ function buttonCreation(symbol) {
   buttonDivId.appendChild(calcButton);
 }
 
-function numberButtonCreation(start, end) {
-  for (let i = start; i <= end; i += 1) {
-    buttonCreation(i);
+// Iterate through the array and create each button
+function numberButtonCreation(list, ID) {
+  for (let i = 0; i < list.length; i += 1) {
+    buttonCreation(list[i], ID);
   }
 }
 
-numberButtonCreation(7, 9);
-buttonCreation('CE');
-numberButtonCreation(4, 6);
-buttonCreation('-');
-numberButtonCreation(1, 3);
-buttonCreation('+');
-buttonCreation(0);
-buttonCreation('.');
-buttonCreation('=');
+// create the numberButtons in the left container
+numberButtonCreation(functionButtonSymbols, 'symbolButtonContainer');
+
+// create the symbolButtons in the right container
+numberButtonCreation(numberButtonSymbols, 'numberButtonContainer');
+
+// create calculation field
+const calcDivId = document.getElementById('calculationFieldContainer');
+
+const calculationField = document.createElement('p');
+calculationField.id = 'calculationFieldText';
+calculationField.textContent = '0';
+
+calcDivId.appendChild(calculationField);
 
 function addition(currentValue, newValue) {
   let addedValue = currentValue + newValue;
