9d3bf06ca4d3804db37c7250d4df3e3070e4928c
sanneVB <sannevanboheemen@gmail.com>
2022-12-13 18:16:04 +0100

    Buttons push to Calculations Window
    A very rough version of updating the calculations window.
    Buttons pushed are added to the window.
    Still need to make all the function keys work

--- calculator.js
+++ calculator.js
@@ -1,5 +1,3 @@
-// const containerId = document.getElementById('numberButtonContainer');
-
 // Creating a list of symbols that should appear on the number buttons
 // Should be listed in the order that they appear from top left
 const numberButtonSymbols = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
@@ -8,27 +6,40 @@ const numberButtonSymbols = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
 // Should be list from top to bottom
 const functionButtonSymbols = ['CE', '-', '+', '='];
 
+const calculationsHistory = [];
+let currentValue = '0';
+
+const buttonPressed = function presser(e) {
+  const paragraphID = document.getElementById('calculationFieldText');
+  if (e.target.parentNode.id === 'numberButtonContainer') {
+    if (currentValue === '0') {
+      currentValue = e.target.innerHTML;
+      paragraphID.innerHTML = currentValue;
+    } else {
+      currentValue += `${e.target.innerHTML}`;
+      paragraphID.innerHTML = currentValue;
+    }
+    calculationsHistory.push(`${e.target.innerHTML}`);
+  }
+  calculationsHistory.push(`${e.target.innerHTML}`);
+  paragraphID.innerHTML = currentValue;
+};
+
 // Function that will take symbol input and then create a button with that symbol
 function buttonCreation(symbol, targetID) {
   const containerId = document.getElementById(targetID);
-  // Creating seperate divs for each button to live
-  const buttonDiv = document.createElement('div');
-  buttonDiv.classList.add('button-div');
-  buttonDiv.id = `button${symbol}`;
-
-  containerId.appendChild(buttonDiv);
-
-  // Then creating the button inside each of those divs
-  const buttonDivId = document.getElementById(`button${symbol}`);
 
   const calcButton = document.createElement('button');
   calcButton.textContent = `${symbol}`;
+  calcButton.id = `button${symbol}`;
+  calcButton.addEventListener('click', buttonPressed);
   calcButton.classList.add('button');
 
-  buttonDivId.appendChild(calcButton);
+  containerId.appendChild(calcButton);
 }
 
-// Iterate through the array and create each button
+// const buttonSummaries = [];
+// Iterate through the array and create each button in their own container
 function numberButtonCreation(list, ID) {
   for (let i = 0; i < list.length; i += 1) {
     buttonCreation(list[i], ID);
@@ -36,7 +47,7 @@ function numberButtonCreation(list, ID) {
 }
 
 // create the numberButtons in the left container
-numberButtonCreation(functionButtonSymbols, 'symbolButtonContainer');
+numberButtonCreation(functionButtonSymbols, 'functionButtonContainer');
 
 // create the symbolButtons in the right container
 numberButtonCreation(numberButtonSymbols, 'numberButtonContainer');
@@ -46,40 +57,20 @@ const calcDivId = document.getElementById('calculationFieldContainer');
 
 const calculationField = document.createElement('p');
 calculationField.id = 'calculationFieldText';
-calculationField.textContent = '0';
+calculationField.textContent = `${currentValue}`;
 
 calcDivId.appendChild(calculationField);
 
-function addition(currentValue, newValue) {
-  let addedValue = currentValue + newValue;
-  addedValue = addedValue.toFixed(2);
-  return addedValue;
-}
+// function displayValue()
 
-function subtraction(currentValue, newValue) {
-  let subractedValue = currentValue - newValue;
-  subractedValue = subractedValue.toFixed(2);
-  return subractedValue;
-}
-
-// Don't need this seeing only button preses will be processed
-// function numberCheck(newValue) {
-//   const typeCheck = (typeof newValue)
-//   if (typeCheck !== number) {
-//     window.alert('Please enter a number')
-//   }
+// function addition(currentValue, newValue) {
+//   let addedValue = currentValue + newValue;
+//   addedValue = addedValue.toFixed(2);
+//   return addedValue;
 // }
 
-// Tracking tile clicked by ID seems more logical
-// tiles.forEach(
-//   // Tracks all the tiles and their index (0-8)
-//   (tileDivElement, i) =>
-//   // Tracks which tile has been clicked and returns the index
-//     tileDivElement.addEventListener('click', ((e) => {
-//       // Function that changes the input field
-//       addInput(i);
-//     })),
-// );
-
-// console.log(addition(500.1547, -30156.15489496));
-// console.log(subtraction(500.1547, -30156.15489496));
+// function subtraction(currentValue, newValue) {
+//   let subractedValue = currentValue - newValue;
+//   subractedValue = subractedValue.toFixed(2);
+//   return subractedValue;
+// }
